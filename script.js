1415d1d3accd8251146730bec96033b9f6b5c818
Krister Viirsaar <git@krister.ee>
2021-10-05 10:52:17 +0000

    This is how to draw a circle

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const canvas = document.querySelector('canvas')
 const context = canvas.getContext('2d')
+
+context.beginPath();
+context.arc(50, 50, 20, 0, 2 * Math.PI);
+context.fillStyle = 'green';
+context.fill();
