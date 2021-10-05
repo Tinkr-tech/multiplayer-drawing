e2fed408374d8d84507557507ea863092fb0957a
Krister Viirsaar <git@krister.ee>
2021-10-05 11:06:49 +0000

    Cleanup: remove the 3 drawCircle commands and console.log() too

--- script.js
+++ script.js
@@ -7,11 +7,7 @@ function drawCircle(x, y, color) {
     context.fillStyle = color;
     context.fill();
 }
-drawCircle(100, 150, 'red')
-drawCircle(100, 100, 'green')
-drawCircle(150, 100, 'blue')
 
 canvas.onclick = function (event) {
-    console.log("Mouse was clicked:", event.offsetX, event.offsetY)
     drawCircle(event.offsetX, event.offsetY, 'black')
 }
