31a9642adca770d16e3cd5fffeda82d98b886a88
Krister Viirsaar <git@krister.ee>
2021-10-05 11:38:17 +0000

    Fetch server data and see what's inside

--- script.js
+++ script.js
@@ -12,8 +12,11 @@ canvas.onclick = function (event) {
     drawCircle(event.offsetX, event.offsetY, 'black')
 }
 
+const databaseURL = "https://tinkr.tech/sdb/drawing-game2"
 async function drawServerData() {
     console.log('This runs every second')
+    const serverData = await fetchGET(databaseURL)
+    console.log("Server data fetched:", serverData.length, serverData[0])
 }
 
 setInterval(drawServerData, 1000)
